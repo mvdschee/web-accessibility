73d59630f29b8191434a1b4d2f59fa0a54f2fc1d
Max van der Schee <m.v.d.schee@ewake.nl>
2019-02-10 12:01:04 +0100

    Added support for React #13

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -37,7 +37,10 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for HTML documents
-		documentSelector: [{ scheme: 'file', language: 'html' }],
+		documentSelector: [
+			{ language: 'html', scheme: 'file' },
+			{ language: 'javascriptreact', scheme: 'file' }
+		],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
