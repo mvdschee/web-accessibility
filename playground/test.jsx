9f1987c46fd725485a62384326ac69b498904f51
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-03-09 15:48:22 +0800

    added way to test code changes

---
+++ playground/test.jsx
@@ -0,0 +1,8 @@
+export default function test() {
+	return (
+		<head>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+		</head>
+	);
+}
---
+++ playground/test.vue
@@ -0,0 +1,15 @@
+<template>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+	</head>
+
+	<div>
+		<html5-custom-tag />
+	</div>
+</template>
+<script lang="ts" setup>
+// ...
+const myTag: Ref<HTMLDivElement | null>(null);
+// ...
+</script>
